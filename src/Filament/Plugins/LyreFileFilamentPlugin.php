739a200a6d30ff01f33efb10ca559cdd211e3958
kigathi-chege <chegekigathi@gmail.com>
2025-05-05 15:52:40 +0300

    feat: prefixed api routes

--- src/Filament/Plugins/LyreFileFilamentPlugin.php
+++ src/Filament/Plugins/LyreFileFilamentPlugin.php
@@ -4,11 +4,14 @@ namespace Lyre\File\Filament\Plugins;
 
 use Filament\Contracts\Plugin;
 use Filament\Panel;
-use Symfony\Component\Finder\Finder;
-use Filament\Resources\Resource;
 
 class LyreFileFilamentPlugin implements Plugin
 {
+    public static function make(): static
+    {
+        return app(static::class);
+    }
+
     public function getId(): string
     {
         return 'lyre.file';
@@ -16,36 +19,12 @@ class LyreFileFilamentPlugin implements Plugin
 
     public function register(Panel $panel): void
     {
-        $resources = self::retrieveFilamentResources();
-
-        $panel
-            ->resources($resources);
+        $resources = get_filament_resources_for_namespace('Lyre\\File\\Filament\\Resources');
+        $panel->resources($resources);
     }
 
     public function boot(Panel $panel): void
     {
         //
-    }
-
-    private static function retrieveFilamentResources()
-    {
-        $resourceNamespace = 'Lyre\\File\\Filament\\Resources';
-        $resourcePath = realpath(__DIR__ . '/../Resources');
-
-        $resources = collect((new Finder)->files()->in($resourcePath)->name('*.php'))
-            ->map(function ($file) use ($resourceNamespace, $resourcePath) {
-                // Get the relative path from resourcePath
-                $relativePath = str_replace('.php', '', $file->getRelativePathname());
-
-                // Convert slashes to namespace separators
-                $class = $resourceNamespace . '\\' . str_replace('/', '\\', $relativePath);
-
-                return class_exists($class) && is_subclass_of($class, Resource::class) ? $class : null;
-            })
-            ->filter()
-            ->values()
-            ->toArray();
-
-        return $resources;
     }
 }
--- src/routes/api.php
+++ src/routes/api.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::apiResources([
-    'files' => \Lyre\File\Http\Controllers\FileController::class,
-]);
+Route::prefix('api')->group(function () {
+    Route::apiResources([
+        'files' => \Lyre\File\Http\Controllers\FileController::class,
+    ]);
 
-Route::get('/files/stream/{slug}.{extension}', [\Lyre\File\Http\Controllers\FileController::class, 'stream'])->name('stream');
-Route::get('/files/download/{slug}.{extension}', [\Lyre\File\Http\Controllers\FileController::class, 'download'])->name('download');
+    Route::get('/files/stream/{slug}.{extension}', [\Lyre\File\Http\Controllers\FileController::class, 'stream'])->name('stream');
+    Route::get('/files/download/{slug}.{extension}', [\Lyre\File\Http\Controllers\FileController::class, 'download'])->name('download');
+});
