64525f6d780502cb8138c3e3deca7ef36efecf83
kigathi-chege <chegekigathi@gmail.com>
2025-05-09 22:29:18 +0300

    feat: hidden file items

--- src/Models/File.php
+++ src/Models/File.php
@@ -9,6 +9,24 @@ class File extends Model
 {
     use HasFactory;
 
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+        'slug',
+        "path",
+        "path_sm",
+        "path_md",
+        "path_lg",
+        "size",
+        "extension",
+        "mimetype",
+        "usagecount",
+        "checksum",
+        "viewed_at",
+        "storage",
+        "laravel_through_key",
+    ];
+
     public function attachments()
     {
         return $this->hasMany(Attachment::class);
