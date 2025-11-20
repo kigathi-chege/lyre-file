fea7362d9ccfdb0be36b272f9b87398202a7146a
kigathi-chege <chegekigathi@gmail.com>
2025-11-20 09:33:51 +0300

    feat: add api middleware

--- src/routes/api.php
+++ src/routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('api')
-    ->middleware(\Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class)
+    ->middleware(['api', \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class])
     ->group(function () {
         Route::apiResources([
             'files' => \Lyre\File\Http\Controllers\FileController::class,
